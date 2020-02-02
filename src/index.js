e87a505d37c3f40288a8a5dfee9c0e4665682892
meghanmcronin <44204885+meghanmcronin@users.noreply.github.com>
2020-02-01 23:59:59 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
+import { SketchPicker } from 'react-color';
 
-var element = React.createElement('h1', { className: 'greeting' });
-ReactDOM.render(element, document.getElementById('root'));
+
+  /*
+  import ImageMapper from 'react-image-mapper';
+
+var element = <ImageMapper src="map.jpg"/>
+ReactDOM.render(element, document.getElementById('parent'));
+  for(var i = 0; i < 10; i++)
+  {
+    document.body.appendChild(toilet);
+  }
+  */
+//var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+//ReactDOM.render(element, document.getElementById('root'));
+
 serviceWorker.unregister();
